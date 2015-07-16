95575eada4c528242b797cf5398e6a5435507063
CTumminaro <CTumminaro@gmail.com>
2015-07-16 11:56:04 -0500

    removed gulp minification and removed $$$image_root$$$ references in background images

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/gulp/tasks/sass.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/gulp/tasks/sass.js
@@ -14,7 +14,7 @@ gulp.task('sass', function () {
         .pipe(sass(config.settings))
         .on('error', handleErrors)
         .pipe(cssImport())
-        .pipe(minifyCSS())
+        //.pipe(minifyCSS())
         //.pipe(sourcemaps.write())
         //.pipe(autoprefixer({ browsers: ['last 2 version'] }))
         .pipe(gulp.dest(config.dest));
