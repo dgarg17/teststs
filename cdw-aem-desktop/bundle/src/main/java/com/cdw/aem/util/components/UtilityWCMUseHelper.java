08d4065cb6b4bdcf519731edcc9d28c582c12c55
goutham <goutved@cdw.com>
2015-08-17 14:38:05 -0500

    checked in product picker story

--- cdw-aem-desktop/bundle/src/main/java/com/cdw/aem/util/components/UtilityWCMUseHelper.java
+++ cdw-aem-desktop/bundle/src/main/java/com/cdw/aem/util/components/UtilityWCMUseHelper.java
@@ -12,57 +12,65 @@ import com.day.cq.wcm.api.Page;
  *
  */
 public class UtilityWCMUseHelper extends BaseComponent {
-	
+
 	private String siteRootPagePath;
 	private String siteRootInheritedProperty;
 	private Page siteRootPage;
 	private String formatPhoneNumber;
-	
+
 	private static String SITE_DOMAIN_PROPERTY = "siteDomain";
 	private static String S7_CDW_PARTNER_LOGO_TREATMENT_PROPERTY = "s7CDWPartnerLogoTreatment";
-	
+	private static String SERVICE_DOMAIN = "serviceDomain";
+	private static String PRODUCT_SERVICE = "productService";
+
 	private boolean isPublish =false;
-	
+
 	@Override
 	public void activate() throws Exception {
 		String propertyName = get("propertyName", String.class);
 		String defaultValue = get("defaultValue",String.class);
 		String phoneNumber = get("phoneno",String.class);
-			
+
 		if(getWcmMode().isDisabled()){
 			isPublish =true;
 		}
 		siteRootPage = Utility.getSiteRootPage(getCurrentPage());
-		
+
 		siteRootPagePath = Utility.getSiteRootPath(getCurrentPage());
-		
+
 				formatPhoneNumber = Utility.formatPhoneNumber(phoneNumber);
-		
+
 		siteRootInheritedProperty = null;
 		if(propertyName!=null ){
-			siteRootInheritedProperty = Utility.getSiteRootInheritedProperty(getCurrentPage().getContentResource(), 
+			siteRootInheritedProperty = Utility.getSiteRootInheritedProperty(getCurrentPage().getContentResource(),
 											propertyName, defaultValue);
 		}
 
 	}
-	
+
 	public String getSiteRootPagePath(){
 		return siteRootPagePath;
 	}
-			
+
 	public String getSiteDomainInheritedProperty(){
 		return Utility.getSiteRootInheritedProperty(getCurrentPage().getContentResource(), SITE_DOMAIN_PROPERTY, "");
 	}
-	
+
 	public String getS7CDWPartnerLogoTreatmentInheritedProperty(){
 		return Utility.getSiteRootInheritedProperty(getCurrentPage().getContentResource(), S7_CDW_PARTNER_LOGO_TREATMENT_PROPERTY, "");
 	}
-	
+
 	public Page getSiteRootPage() {
 		return siteRootPage;
 	}
 	public String getFormatPhoneNumber(){
 		return formatPhoneNumber;
 	}
-	
+
+	public String getServiceDomain(){
+		return  Utility.getSiteRootInheritedProperty(getCurrentPage().getContentResource(), SERVICE_DOMAIN, "");
 	}
+	public String getProductService(){
+		return  Utility.getSiteRootInheritedProperty(getCurrentPage().getContentResource(), PRODUCT_SERVICE, "");
+	}
+}
