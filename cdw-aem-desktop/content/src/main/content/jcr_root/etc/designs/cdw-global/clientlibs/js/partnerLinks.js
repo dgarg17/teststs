4fa041fa43c2d9600cf44d8d6c6c42846536aa92
loganchan89 <tyson.je1991@gmail.com>
2015-09-11 16:20:24 -0500

    added styles for solution nav changed ID's to classes adjusted css and js accordingly

---
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/partnerLinks.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+	partnerLinks();
+});
+
+var partnerLinks = function() {
+	$(".partner-links").change(function() {
+		if ($(this).find(':selected').data('target') == "_blank") {
+			window.open($(this).find(':selected').data('link'),'_blank');
+		}
+		else {
+			window.location=$(this).find(':selected').data('link');
+		}
+	});
+}
