543cdb95c6a6066bac0f5c58ae53cd23e26c0c1e
goutham <goutved@cdw.com>
2015-09-23 15:18:21 -0500

    youtube API :  percentage   temporary  fix

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/youtube.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/youtube.js
@@ -6,29 +6,23 @@
 
 
 function onYouTubePlayerAPIReady() {
-console.log("youtube ready");
+    console.log("youtube ready");
 	$(".cdw-youtube-video").each(function(index,value) {
 
 		var youtubePlayerID = $(this).data("video-id");
 		var trackingId = $(this).data("video-tracking-id");
-
-		console.log("youtube  video "+ youtubePlayerID+":"+trackingId);
-		console.log("youtube  video  width "+ $(this).data("video-width")+$(this).data("video-size-unit"));
-		console.log("youtube  video  height "+ parseInt(parseInt($(this).data("video-width"))*parseFloat($(this).data("video-aspect-ratio")))+$(this).data("video-size-unit"));
 		new YT.Player(this.id, {
-			//width: $(this).data("video-width")+$(this).data("video-size-unit"),
-			//width: $(this).data("video-width")+$(this).data("video-size-unit"),
-			//height: parseInt(parseInt($(this).data("video-width"))*parseFloat($(this).data("video-aspect-ratio")))+$(this).data("video-size-unit"),
-			height: "600px",
+			width: $(this).data("video-width")+$(this).data("video-size-unit"),
+			height:($(this).data("video-size-unit")=="%")?null:parseInt(parseInt($(this).data("video-width"))*parseFloat($(this).data("video-aspect-ratio")))+"px",
 			videoId: $(this).data("video-id"),
 			events: {
 				'onStateChange': function(event){
 					if (event.data == 1) {
-						console.log("youtube  event change play "+ youtubePlayerID+": "+trackingId);
+					//	console.log("youtube  event change play "+ youtubePlayerID+": "+trackingId);
 					//	CdwTagMan.createPageViewTag('Video', 'Video Played | ' + trackingId);
 					}
 					if (event.data == 0) {
-						console.log("youtube  event change play "+ youtubePlayerID+ ": "+trackingId) ;
+					//	console.log("youtube  event change play "+ youtubePlayerID+ ": "+trackingId) ;
 					//	CdwTagMan.createPageViewTag('Video', 'Video Ended | ' + trackingId);
 					}
 				}
