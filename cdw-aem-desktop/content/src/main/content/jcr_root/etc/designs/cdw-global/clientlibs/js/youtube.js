f50332541f7a641fd70eb5d3d46e46b1cca114c9
goutham <goutved@cdw.com>
2015-09-24 14:40:22 -0500

    sub category product picker: created new component

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/youtube.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/youtube.js
@@ -6,14 +6,23 @@
 
 
 function onYouTubePlayerAPIReady() {
-    console.log("youtube ready");
+   // console.log("youtube ready");
 	$(".cdw-youtube-video").each(function(index,value) {
-
-		var youtubePlayerID = $(this).data("video-id");
-		var trackingId = $(this).data("video-tracking-id");
+		var width=parseInt($(this).data("video-width"));
+		var height;
+        console.log("width from AEM "+width);
+		if($(this).data("video-size-unit")=="%"){
+			width=$(this).parent().width()*(parseInt($(this).data("video-width"))/100);
+		//	console.log("width after calculation "+width);
+		//	console.log("width of the parent "+$(this).parent().width());
+		}
+	    height=parseInt(width*parseFloat($(this).data("video-aspect-ratio")));
+	    //console.log("height after calcualtion  "+height);
+		//var youtubePlayerID = $(this).data("video-id");
+		//var trackingId = $(this).data("video-tracking-id");
 		new YT.Player(this.id, {
-			width: $(this).data("video-width")+$(this).data("video-size-unit"),
-			height:($(this).data("video-size-unit")=="%")?null:parseInt(parseInt($(this).data("video-width"))*parseFloat($(this).data("video-aspect-ratio")))+"px",
+			width: width+"px",
+			height:height+"px",
 			videoId: $(this).data("video-id"),
 			events: {
 				'onStateChange': function(event){
