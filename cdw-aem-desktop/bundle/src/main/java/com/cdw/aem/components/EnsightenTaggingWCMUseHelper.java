52bd0cafacf54af369d80dd99b02246f32edfb70
goutham <goutved@cdw.com>
2015-10-15 12:26:33 -0500

    Analytic  in page tagging: updated WCM use class name change

--- cdw-aem-desktop/bundle/src/main/java/com/cdw/aem/components/EnsightenTaggingWCMUseHelper.java
+++ cdw-aem-desktop/bundle/src/main/java/com/cdw/aem/components/EnsightenTaggingWCMUseHelper.java
@@ -5,7 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 
-public class TaggingWCMUseHelper extends WCMUse {
+public class EnsightenTaggingWCMUseHelper extends WCMUse {
 
     protected Logger log = LoggerFactory.getLogger(this.getClass());
 
