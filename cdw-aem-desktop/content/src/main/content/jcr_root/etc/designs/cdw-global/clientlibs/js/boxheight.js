871f798b25ba0bd997e60718940369e075eba1bf
loganchan89 <tyson.je1991@gmail.com>
2015-11-03 16:11:56 -0600

    fixed red-bar highlight treatment fixed defect with buttons adjusted hero

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/boxheight.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/boxheight.js
@@ -10,28 +10,32 @@ $(document).ready(function(){
 
 var resizeMultiupBoxes = function() {
 
-	//var $fixHeight = $('.-box-height').parent();
-	/*
-	$fixHeight.each(function() {
-		$fixHeight.css('height', '').find('.-box-height').css('height', '');
-	});
-	$fixHeight.promise().done(function() {
-		if ($(window).width() > 735) {
-			$(this).find('.-box-height').css('height', $(this).parents('.layout').height() - 50);
-			$(this).find('.cdw-youtube-video').css('height', $(this).parents('.layout').height() - 20);
-			$(this).find('.media iframe').css('height', $(this).parents('.layout').height() - 20);
-			$(this).css('height', $(this).parents('.layout').height());
-		}
-		else {
-			$(this).css('height', '').find('.-box-height').css('height', '');
-		}
-	});
-	*/
-	
 	$(".-box-height").each(function() {
-		$(this).parent().parent().addClass("box-height-container");
+		$(this).parent().parent().parent().addClass("-box-height-container");
+        //var theHeight = $(this).parent().parent().height();
+       // alert(theHeight);
 	});
-	
+
+	$(".-box-height-container").each(function() {
+
+        	var maxHeight = 0;
+            $(this).children().each(function() {
+				if ($(this).children().children().height() > maxHeight) {
+					maxHeight = $(this).children().children().height();
+                }
+            });
+            $(this).children().each(function() {
+            	if ($(this).children().children(".-box-height").hasClass("youtube-wrapper")) {
+					$(this).children().children(".-box-height").children().height(maxHeight+39);
+                }
+                else {
+					$(this).children().children(".-box-height").height(maxHeight);
+                }
+            });
+
+
+	});
+
 	
 	
 }
@@ -41,6 +45,3 @@ var alignButtons = function() {
 
 
 }
-
-
-
