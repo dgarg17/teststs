b3e4e33f0c70139cd1a2babf6c16b51884f46776
loganchan89 <tyson.je1991@gmail.com>
2015-11-04 14:59:49 -0600

    merging js to master

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/boxheight.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/boxheight.js
@@ -4,29 +4,6 @@ $(document).ready(function(){
 	
 	$(window).resize(function() {
 		resizeMultiupBoxes();
-<<<<<<< HEAD
-	});
-	
-});
-
-var resizeMultiupBoxes = function() {
-
-	var $fixHeight = $('.-box-height').parent();
-	$fixHeight.each(function() {
-		$fixHeight.css('height', '').find('.-box-height').css('height', '');
-	});
-	$fixHeight.promise().done(function() {
-		if ($(window).width() > 735) {
-			$(this).find('.-box-height').css('height', $(this).parents('.layout').height() - 50);
-			$(this).find('.cdw-youtube-video').css('height', $(this).parents('.layout').height() - 20);
-			$(this).find('.media iframe').css('height', $(this).parents('.layout').height() - 20);
-			$(this).css('height', $(this).parents('.layout').height());
-		}
-		else {
-			$(this).css('height', '').find('.-box-height').css('height', '');
-		}
-	});
-=======
 	});
 	
 });
@@ -61,18 +38,10 @@ var resizeMultiupBoxes = function() {
 
 	
 	
->>>>>>> develop
 }
 
 var alignButtons = function() {
 
 
 
-<<<<<<< HEAD
 }
-
-
-
-=======
-}
->>>>>>> develop
