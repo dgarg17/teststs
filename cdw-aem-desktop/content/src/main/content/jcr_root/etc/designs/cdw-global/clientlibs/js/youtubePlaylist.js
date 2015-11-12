c794575a6c1590fd814f9a7213c07529e7315022
christebbens <chris.tebbens@gmail.com>
2015-11-12 16:10:38 -0600

    Ran Gulp Production to move css and js to AEM files

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/youtubePlaylist.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/youtubePlaylist.js
@@ -1,61 +1,80 @@
-var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+var youtubePlaylist = function (thisPlayer) {
+	this.buildPlayer(thisPlayer);
+	this.setupPlayer(thisPlayer);
+};
 
-function onYouTubePlayerAPIReady() {
-   // console.log("youtube api ready");
-}
-
-function setupYouTubePlaylist() {
-	$('.video-thumb:first').addClass('active');
-	//$('.video-thumb').append("<div class='arrow-down'></div>");
-	$(".ico-play-circle-o").click(function() {
-		$(".video-hero-image-container").hide();
-		$(".youtube-playlist-video").show();
-	});
-	$(".video-thumb").click(function() {
-		$(".video-thumb").removeClass("active");
-		$(this).addClass("active");
-		switchYouTubePlaylist($(this));
-	});
-	
-	resizeYouTubePlaylistPlayer();
-}
-
-function switchYouTubePlaylist(element) {
-	$(".youtube-playlist-video").hide();
-	$(".video-hero-image-container").show();
-	$(".video-hero-image").attr("src",element.data("hero-image"));
-	$(".video-hero-image-container h2").html(element.data("video-headline-text"));
-	$(".youtube-playlist-video").attr("src","https://www.youtube.com/embed/" + element.data("video-id") + "?enablejsapi=1&amp;origin=http%3A%2F%2Fwww.cdw.com%3A4502");
-	$(".youtube-playlist-video").attr("id",element.data('video-id'));
-	$(".youtube-playlist-video").attr("data-tracking-id",element.data('tracking-id'));
-	$(".youtube-playlist-video").attr("title",element.data('video-title'));
-	$(".youtube-playlist-video").attr("data-video-summary",element.data('video-summary'));
-	$(".youtube-playlist-video").attr("data-video-width",element.data('video-width'));
-	$(".youtube-playlist-video").attr("data-video-size-unit",element.data('video-size-unit'));
-	$(".youtube-playlist-video").attr("data-video-aspect-ratio",element.data('video-aspect-ratio'));
-	$(".video-hero-text-container h2").html(element.data('video-headline-text'));
-	$(".video-hero-text-container .video-title").html(element.data('video-title'));
-	$(".video-hero-text-container .video-summary").html(element.data('video-summary'));
-	resizeYouTubePlaylistPlayer();
-}
-
-function buildYouTubePlaylistPlayer() {
-	var firstVideo = $(".video-thumb").first();
-	$('.video-hero').append('<iframe class="youtube-playlist-video" id="' + firstVideo.data('video-id') + '" data-video-id="' + firstVideo.data('video-id') + '" data-video-width="' + firstVideo.data('video-width') + '" data-video-size-unit="' + firstVideo.data('video-size-unit') + '" data-video-aspect-ratio="' + firstVideo.data('video-aspect-ratio') + '" data-tracking-id="' + firstVideo.data('tracking-id') + '" data-video-summary="' + firstVideo.data('video-summary') + '" frameborder="0" allowfullscreen="1" title="' + firstVideo.data('video-title') + '"  src="https://www.youtube.com/embed/' + firstVideo.data('video-id') + '?enablejsapi=1&amp;origin=http%3A%2F%2Fwww.cdw.com%3A4502"></iframe>');
+youtubePlaylist.prototype.buildPlayer = function(thisPlayer) {
+	var firstVideo = thisPlayer.find(".video-thumb").first();
+	var globalVideoSettings = thisPlayer.find(".global-video-settings");
+	$('.video-hero').append('<iframe class="youtube-playlist-video" id="' + firstVideo.data('video-id') + '" data-video-id="' + firstVideo.data('video-id') + '" data-video-width="' + globalVideoSettings.data('video-width') + '" data-video-size-unit="' + globalVideoSettings.data('video-size-unit') + '" data-video-aspect-ratio="' + globalVideoSettings.data('video-aspect-ratio') + '" data-tracking-id="' + firstVideo.data('tracking-id') + '" data-video-summary="' + firstVideo.data('video-summary') + '" frameborder="0" allowfullscreen="1" title="' + firstVideo.data('video-title') + '"  src="https://www.youtube.com/embed/' + firstVideo.data('video-id') + '?enablejsapi=1&amp;origin=http%3A%2F%2Fwww.cdw.com%3A4502"></iframe>');
 	$(".video-hero-text-container h2").html(firstVideo.data('video-headline-text'));
 	$(".video-hero-text-container .video-title").html(firstVideo.data('video-title'));
 	$(".video-hero-text-container .video-summary").html(firstVideo.data('video-summary'));
 }
 
-function resizeYouTubePlaylistPlayer() {
-	$(".video-hero iframe").width($(".video-hero-image-container").width());
-	$(".video-hero iframe").height($(".video-hero-image-container").height());
+youtubePlaylist.prototype.formatUrl = function(url) {
+	return "https://www.youtube.com/embed/" + url + "?enablejsapi=1&amp;origin=http%3A%2F%2Fwww.cdw.com%3A4502";
 }
 
-if ($(".video-thumb").length && $(".video-thumb").data("video-id")) {
-	buildYouTubePlaylistPlayer();
-	setupYouTubePlaylist();
+youtubePlaylist.prototype.resizePlayer = function(thisPlayer) {
+	thisPlayer.find(".video-hero iframe").width($(".video-hero-image-container").width());
+	thisPlayer.find(".video-hero iframe").height($(".video-hero-image-container").height());
 }
+
+youtubePlaylist.prototype.setupPlayer = function(thisPlayer) {
+	var that = this;
+	thisPlayer.find('.video-thumb').first().addClass('active');
+	$(".ico-play-circle-o").click(function() {
+		thisPlayer.find(".video-hero-image-container").hide();
+		thisPlayer.find(".youtube-playlist-video").show();
+	});
+	$(".video-thumb").click(function() {
+		thisPlayer.find(".video-thumb").removeClass("active");
+		$(this).addClass("active");
+		that.swapPlayer($(this),thisPlayer);
+	});
+	this.resizePlayer(thisPlayer);
+}
+
+youtubePlaylist.prototype.swapPlayer = function(element, thisPlayer) {
+	thisPlayer.find(".youtube-playlist-video").hide();
+	thisPlayer.find(".video-hero-image-container").show();
+	
+	// Set Background Image
+	thisPlayer.find(".video-hero-image").attr("src",element.data("hero-image"));
+	
+	// Set Data Attributes
+	thisPlayer.find(".youtube-playlist-video").attr("src",this.formatUrl(element.data("video-id")));
+	thisPlayer.find(".youtube-playlist-video").attr("id",element.data('video-id'));
+	thisPlayer.find(".youtube-playlist-video").attr("title",element.data('video-title'));
+	thisPlayer.find(".youtube-playlist-video").attr("data-tracking-id",element.data('tracking-id'));
+	
+	
+	// Set HTML Contents
+	var htmlElements = ["video-headline-text","video-title","video-summary"];
+	for (var i=0; i<htmlElements.length; i++) {
+		if (element.data(htmlElements[i])) {
+			thisPlayer.find(".video-hero-text-container ." + htmlElements[i]).html(element.data(htmlElements[i]));
+		}
+		else {
+			thisPlayer.find(".video-hero-text-container ." + htmlElements[i]).html("");
+		}
+	}
+
+	this.resizePlayer(thisPlayer);
+}
+
+// Check that the element is found and has data, then instantiate
+/*
+if ($(".video-thumb").length && $(".video-thumb").data("video-id")) {
+	var youtubePlaylist1 = new youtubePlaylist();
+}
+*/
+
+var youtubePlaylistPlayer = [];
+
+$.each($(".youtube-playlist-player"), function(index, value) {
+	if ($(this).find($(".video-thumb")).data("video-id")) {
+		youtubePlaylistPlayer[index] = new youtubePlaylist($(this));
+	}
+});
