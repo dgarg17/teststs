eef1146183e311276d3509f0a79111d704ee5664
CTumminaro <CTumminaro@gmail.com>
2015-11-16 14:53:53 -0600

    added social share module outside FED JS framework

--- fed-aem/app/js/src/teams/common/main.js
+++ fed-aem/app/js/src/teams/common/main.js
@@ -13,46 +13,7 @@ window.CDW = {};
 CDW.productPlacement = productPlacement; 
 
 $(function() {
-	/* Partner Solution Nav */
-	$('header').find('.nav-collapsed').on('click', function() {
-		$(this).next('.nav').slideToggle(300);
-	});
-	$(window).resize(function() {
-		$('header').find('.nav').css('display','');
-		if ($(window).width() > 970) {
-			$('.partner-menu').show().css('display', '');
-		}
-	});
-
-	//Partner Solution Nav Mobile Menu
-	$(document).on('click', '.partner-header .mobileNav', togglePartnerMenu);
-	
 	//Social Share
     socialShare.init();
 
 });
-
-//Pin Partner Nav to top on scoll
-$(window).scroll(function () {
-	if ($('.partner-header-wrapper').length) {
-	console.log("partner header wrapper found");
-		if ($(window).scrollTop() > $('.partner-header-wrapper').offset().top) {
-			$('.partner-header').addClass('stick');
-		} else {
-			$('.partner-header').removeClass('stick');
-		}
-	}
-});
-
-var togglePartnerMenu = function() {
-	switch ($('.partner-menu').is(':visible')) {
-		case true:
-			$('.mobileNav').removeClass('ico-uni52').addClass('ico-uni71');
-			$('.partner-menu').slideUp();
-			break;
-		case false:
-			$('.mobileNav').removeClass('ico-uni71').addClass('ico-uni52');
-			$('.partner-menu').slideDown();
-			break;
-	}
-}
