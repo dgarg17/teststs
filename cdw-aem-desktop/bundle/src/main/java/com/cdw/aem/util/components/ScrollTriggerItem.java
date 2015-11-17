d2163f6e3e8ee5656ff8330d97dbd79dd4dd148c
Will Carpenter <wcarpent@adobe.com>
2015-11-17 09:19:20 -0800

    Squashed commit of the following:
    commit 6f4105e6bbd0fa6f7b59862594ace4aee9fdf059
    Author: Will Carpenter <wcarpent@adobe.com>
    Date:   Tue Nov 10 08:26:58 2015 -0800
    
        Committing OnScroll functionality
    
        Signed-off-by: Will Carpenter <wcarpent@adobe.com>
    
    commit e7b9f6457a078f78b75db31586cbcce83f9a075f
    Author: christebbens <chris.tebbens@gmail.com>
    Date:   Sun Nov 8 15:06:25 2015 -0600
    
        Added scrollTriggers.js into clientlibs
    
    Signed-off-by: Will Carpenter <wcarpent@adobe.com>

---
+++ cdw-aem-desktop/bundle/src/main/java/com/cdw/aem/util/components/ScrollTriggerItem.java
@@ -0,0 +1,22 @@
+package com.cdw.aem.util.components;
+
+public class  ScrollTriggerItem {
+	private String tagName;
+	private String elementName;
+	
+	public String getTagName() {
+		return tagName;
+	}
+	
+	public void setTagName(String tagName) {
+		this.tagName = tagName;
+	}
+	
+	public String getElementName() {
+		return elementName;
+	}
+	
+	public void setElementName(String elementName) {
+		this.elementName = elementName;
+	}
+}
