77e02d1db70806b9e4cf97c4f89fb02230edbd3a
goutham <goutved@cdw.com>
2015-11-27 13:06:28 -0600

    Model : updated model

--- cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/partnerLinks.js
+++ cdw-aem-desktop/content/src/main/content/jcr_root/etc/designs/cdw-global/clientlibs/js/partnerLinks.js
@@ -4,11 +4,21 @@ $(document).ready(function(){
 
 var partnerLinks = function() {
 	$(".partner-links").change(function() {
+
+
+		if(!($(this).find(':selected').data('change')==null)){
+			eval($(this).find(':selected').data('change'));
+		}
+
 		if ($(this).find(':selected').data('target') == "_blank") {
 			window.open($(this).find(':selected').data('link'),'_blank');
 		}
-		else {
-			window.location=$(this).find(':selected').data('link');
+		else if($(this).find(':selected').data('target') == "_model"){
+		console.log("javascript from model "+$(this).find(':selected').data('eventtype') +" "+$(this).find(':selected').data('modeltaggingdata') );
+			cdwModal.open($(this).find(':selected').data('link') , $(this).find(':selected').data('eventtype'), $(this).find(':selected').data('modeltaggingdata'));
+			console.log("javascript from model after "+$(this).find(':selected').data('eventtype') +" "+$(this).find(':selected').data('modeltaggingdata') );
+		}else{
+		window.location=$(this).find(':selected').data('link');
 		}
 	});
 }
